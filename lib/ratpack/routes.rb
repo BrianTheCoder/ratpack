ebc002b230bf14a347433e6f1d119af546a37c71
brianthecoder <wbsmith83@gmail.com>
2009-04-07 14:47:35 -0500

    trying to get it working

---
+++ lib/ratpack/routes.rb
@@ -0,0 +1,12 @@
+module RatPack
+  module Routes
+    def subdomains(tld_len=1) # we set tld_len to 1, use 2 for co.uk or similar
+      subdomain_regex = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/  
+      @subdomains ||= if (request.host.nil? ||subdomain_regex.match(request.host))
+                        request.host.split('.')[0...(1 - tld_len - 2)]
+                      else
+                        []
+                      end
+      end
+  end
+end
